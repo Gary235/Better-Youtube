bac539d8d7a3686c9020594dfef625d63032a0b9
gary.berkman <gary.berkman@wearepsh.com>
2023-08-28 12:00:07 -0300

    update packages

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 
 import store from "@/redux/store";
@@ -7,9 +7,10 @@ import App from "./App";
 
 import "./main.css";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
 );
